d6f361f343983e90ca2e844cfa917cae692d7958
Мадихан Агатанов"git config --global user.name Мадихан <i@madikhan.ru>
2022-08-30 01:14:08 +0500

    add all project

--- api_yamdb/api/utils.py
+++ api_yamdb/api/utils.py
@@ -7,7 +7,7 @@ from rest_framework.views import exception_handler
 
 
 def email_code() -> int:
-    code = "".join(random.choice(string.digits) for _ in range(6))
+    code = ''.join(random.choice(string.digits) for _ in range(6))
     return int(code)
 
 
@@ -18,7 +18,7 @@ def custom_exception_handler(exc, context):
     """
     response = exception_handler(exc, context)
     if response is not None and isinstance(exc, Http404):
-        response.data["detail"] = exc.args[0]
+        response.data['detail'] = exc.args[0]
     return response
 
 
@@ -30,16 +30,9 @@ def send_email(email: str, code: int) -> None:
 
     Returns: None
     """
-    mail_subject = "Код подтверждения для сайта YAMDB"
-    message = (
-        f"Ваш код подтверждения: {code} . "
-        f"Используйте код для активации токена "
-        f"по адресу /api/v1/auth/token/"
-    )
-    send_mail(
-        mail_subject,
-        message,
-        "madikhanagatanov@gmail.com",
-        [email],
-        fail_silently=False,
-    )
+    mail_subject = 'Код подтверждения для сайта YAMDB'
+    message = (f'Ваш код подтверждения: {code} . '
+               f'Используйте код для активации токена '
+               f'по адресу /api/v1/auth/token/')
+    send_mail(mail_subject, message, 'madikhanagatanov@gmail.com',
+              [email], fail_silently=False)
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,12 +1,3 @@
-from django.conf import settings
-from django.shortcuts import get_object_or_404
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import (filters, mixins, pagination, permissions, status,
-                            viewsets)
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import AccessToken
-
 from api.filters import TitleFilter
 from api.mixins import ListCreateDestroyViewSet
 from api.permissions import (IsAdminOrReadOnly, IsAdminOrSuperUser,
@@ -17,40 +8,46 @@ from api.serializers import (CategorySerialiser, CommentSerializer,
                              TitleSerialiser, UserEmailCodeSerializer,
                              UserSerializer)
 from api.utils import email_code, send_email
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import (filters, mixins, pagination, permissions, status,
+                            viewsets)
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken
 from reviews.models import Categories, Genres, Title, User
 
 
 class AdminUserViewSet(viewsets.ModelViewSet):
-    """API for admin actions: get, post, patch, delete users info."""
-
+    """API for admin actions: get, post, patch, delete users info.
+    """
     queryset = User.objects.all()
     serializer_class = FullUserSerializer
     filter_backends = (filters.SearchFilter,)
-    lookup_field = "username"
+    lookup_field = 'username'
     permission_classes = (IsAdminOrSuperUser,)
     pagination_class = pagination.LimitOffsetPagination
-    search_fields = ("username",)
+    search_fields = ('username',)
 
-    @action(
-        detail=False,
-        url_path="me",
-        methods=["GET", "PATCH"],
-        permission_classes=(permissions.IsAuthenticated,),
-    )
+    @action(detail=False, url_path='me', methods=['GET', 'PATCH'],
+            permission_classes=(permissions.IsAuthenticated,))
     def user_get_patch_page(self, request):
+
         user = request.user
-        if request.method == "GET":
+        if request.method == 'GET':
             serializer = self.get_serializer(user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         serializer = self.get_serializer(user, data=request.data, partial=True)
         if serializer.is_valid(raise_exception=True):
             serializer.save(role=user.role, partial=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
+        return None
 
 
 class UserViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
-    """API for user registration with username and email on page auth/signup/."""
-
+    """API for user registration with username and email on page auth/signup/.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
@@ -65,13 +62,13 @@ class UserViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
 
     def perform_create(self, serializer):
         code = email_code()
-        send_email(serializer.validated_data.get("email"), code)
+        send_email(serializer.validated_data.get('email'), code)
         serializer.save(confirmation_code=code)
 
 
 class GetTokenViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
-    """API for generating token response."""
-
+    """API for generating token response.
+    """
     queryset = User.objects.all()
     serializer_class = UserEmailCodeSerializer
 
@@ -80,21 +77,19 @@ class GetTokenViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
         serializer.is_valid(raise_exception=True)
         headers = self.get_success_headers(serializer.data)
         user = get_object_or_404(
-            User, username=serializer.data.get("username")
+            User, username=serializer.data.get('username')
         )
         user.confirmation_code = settings.RESET_CONFIRMATION_CODE
         user.save()
         token = AccessToken.for_user(user)
         return Response(
-            {"token": str(token)}, status=status.HTTP_200_OK, headers=headers
-        )
+            {'token': str(token)}, status=status.HTTP_200_OK, headers=headers)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
-    """This viewset automatically provides `list`, `create`, `retrieve`,
-    `update`, `partial_update` and `destroy` actions.
+    """ This viewset automatically provides `list`, `create`, `retrieve`,
+        `update`, `partial_update` and `destroy` actions.
     """
-
     queryset = Title.objects.all()
     serializer_class = TitleSerialiser
     permission_classes = (IsAdminOrReadOnly,)
@@ -103,35 +98,33 @@ class TitleViewSet(viewsets.ModelViewSet):
     filterset_class = TitleFilter
 
     def get_serializer_class(self):
-        if self.action in ("retrieve", "list"):
+        if self.action in ('retrieve', 'list'):
             return ReadOnlyTitleSerializer
         return TitleSerialiser
 
 
 class GenreViewSet(ListCreateDestroyViewSet):
-    """This viewset automatically provides `list`, `create`, `destroy`
-    actions.
+    """ This viewset automatically provides `list`, `create`,
+    `destroy` actions.
     """
-
     queryset = Genres.objects.all()
     serializer_class = GenreSerialiser
     permission_classes = (IsAdminOrReadOnly,)
     filter_backends = (filters.SearchFilter,)
-    search_fields = ("name",)
-    lookup_field = "slug"
+    search_fields = ('name',)
+    lookup_field = 'slug'
 
 
 class CategoryViewSet(ListCreateDestroyViewSet):
-    """This viewset automatically provides `list`, `create`, `destroy`
-    actions.
+    """ This viewset automatically provides `list`, `create`,
+    `destroy` actions.
     """
-
     queryset = Categories.objects.all()
     serializer_class = CategorySerialiser
     permission_classes = (IsAdminOrReadOnly,)
     filter_backends = (filters.SearchFilter,)
-    search_fields = ("name",)
-    lookup_field = "slug"
+    search_fields = ('name',)
+    lookup_field = 'slug'
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
@@ -139,18 +132,17 @@ class ReviewViewSet(viewsets.ModelViewSet):
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update`, `partial_update` and `destroy` actions.
     """
-
     serializer_class = ReviewSerializer
     permission_classes = (IsAuthorOrReadOnly,)
 
     def get_queryset(self):
-        title = get_title(self.kwargs["title_id"])
-        queryset = title.reviews.all()
-        return queryset
+        title = get_title(self.kwargs['title_id'])
+        return title.reviews.all()
 
     def perform_create(self, serializer):
         serializer.save(
-            author=self.request.user, title=get_title(self.kwargs["title_id"])
+            author=self.request.user,
+            title=get_title(self.kwargs['title_id'])
         )
 
 
@@ -159,20 +151,22 @@ class CommentViewSet(viewsets.ModelViewSet):
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update`, `partial_update` and `destroy` actions.
     """
-
     serializer_class = CommentSerializer
     permission_classes = (IsAuthorOrReadOnly,)
 
     def get_queryset(self):
-        title = get_title(self.kwargs["title_id"])
-        review = get_review(title, self.kwargs["review_id"])
-        queryset = review.comments.all()
-        return queryset
+        title = get_title(self.kwargs['title_id'])
+        review = get_review(title, self.kwargs['review_id'])
+        return review.comments.all()
 
     def perform_create(self, serializer):
-        title = get_title(self.kwargs["title_id"])
-        review = get_review(title, self.kwargs["review_id"])
-        serializer.save(author=self.request.user, title=title, review=review)
+        title = get_title(self.kwargs['title_id'])
+        review = get_review(title, self.kwargs['review_id'])
+        serializer.save(
+            author=self.request.user,
+            title=title,
+            review=review
+        )
 
 
 def get_title(title_id):
